c3908005db531c307e78ad91d550eb4687681c85
Justin Beaulieu <beaulieudev@gmail.com>
2019-02-12 00:38:51 +0000

    Automatically backed up by Learn

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -4,15 +4,16 @@ class Artist < ActiveRecord::Base
 
   def get_genre_of_first_song
     #return the genre of the artist's first saved song
-    Genre.song[1]
+    self.song[1]
   end
 
   def song_count
     #return the number of songs associated with the artist
-    Artist.song.count
+    self.song.count
   end
 
   def genre_count
+    self.genre.count
     #return the number of genres associated with the artist
   end
 end
